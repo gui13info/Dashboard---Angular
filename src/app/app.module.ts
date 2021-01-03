454e49ba57d71a8655a2e84ffccf0bb09e3a7d9c
Guilherme Matos <marques.guilherme0067@gmail.com>
2021-01-03 14:11:14 -0300

    Criando page de consulta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
+import { ResumoComponent } from './resumo/resumo.component';
+import { ConsultaComponent } from './consulta/consulta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelSimplesComponent,
+    BarraNavegacaoComponent,
+    MenuLateralComponent,
+    ResumoComponent,
+    ConsultaComponent
   ],
   imports: [
     BrowserModule,
