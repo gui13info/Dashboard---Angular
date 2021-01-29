47c0df8af03c9a188ce0e5ccf5d15a596d725ab7
Guilherme Matos <marques.guilherme0067@gmail.com>
2021-01-29 20:19:21 -0300

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConsultaComponent } from './consulta/consulta.component';
+import { ResumoComponent } from './resumo/resumo.component';
 
-const routes: Routes = [];
+let routes = [
+  { path : 'resumo', component: ResumoComponent},
+  { path : 'consulta', component: ConsultaComponent },
+  { path : "**" , redirectTo:"/resumo"}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
