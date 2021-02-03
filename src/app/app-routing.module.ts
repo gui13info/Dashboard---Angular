58de52fc31af4f9ffa4c12b941a98f7a82b9e5fa
Guilherme Matos <marques.guilherme0067@gmail.com>
2021-02-03 16:06:17 -0300

    Faturamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ConsultaComponent } from './consulta/consulta.component';
 import { ResumoComponent } from './resumo/resumo.component';
+import { FaturamentoComponent } from './faturamento/faturamento.component';
 
 let routes = [
   { path : 'resumo', component: ResumoComponent},
   { path : 'consulta', component: ConsultaComponent },
+  { path: 'faturamento', component: FaturamentoComponent},
   { path : "**" , redirectTo:"/resumo"}
 ]
 
